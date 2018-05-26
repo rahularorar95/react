954da82761915dd078cf57425ebe78e9a1c4b11a
Rahul Arora <rahularrar95@gmail.com>
2018-05-26 19:05:20 +0530

    new projects added

---
+++ todoapp/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import {createStore} from 'redux'
+import todoApp from './reducers'
+import { Provider } from 'react-redux'
+
+const store = createStore(todoApp,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+console.log(store.getState())
+
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>
+, document.getElementById('root'));
+registerServiceWorker();
